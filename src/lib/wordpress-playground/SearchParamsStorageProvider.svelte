2bad6bd6de656913c307729ed88e777797c84a0e
AJ Moon <alex.jared.moon@gmail.com>
2023-06-07 14:36:11 +0000

    fix: update REPL name in Search params client side

--- src/lib/wordpress-playground/SearchParamsStorageProvider.svelte
+++ src/lib/wordpress-playground/SearchParamsStorageProvider.svelte
@@ -8,15 +8,18 @@
 		PLAYGROUND_WP_VERSION_PARAM,
 		PLAYGROUND_PHP_VERSION_PARAM,
 		PLAYGROUND_URL_PARAM,
+		REPL_NAME_PARAM,
 	} from '$lib/wordpress-playground';
 
 	const {
 		config: {
 			wpUrl,
+			name,
 			stackVersions: { wp, php },
 		},
 	} = getPlaygroundContext();
 
+	// Only use if you want these values to change history
 	$: {
 		if (browser) {
 			const newUrl = new URL($page.url);
@@ -30,5 +33,18 @@
 				keepFocus: true,
 			});
 		}
+	}
+
+	// Similar code expet it doesn't add a History entry
+	$: if (browser) {
+		const newUrl = new URL($page.url);
+
+		newUrl.searchParams.set(REPL_NAME_PARAM, $name);
+
+		goto(newUrl, {
+			noScroll: true,
+			keepFocus: true,
+			replaceState: true,
+		});
 	}
 </script>
