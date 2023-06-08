adc3c54c7c9a3389d245634d568718e3a8e728d6
AJ Moon <alex.jared.moon@gmail.com>
2023-06-08 15:11:30 +0000

    conditional wrapper

---
+++ src/lib/ConditionalWrapper.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import type { ComponentType } from 'svelte';
+
+	export let condition = false;
+	export let tag: string | ComponentType;
+</script>
+
+{#if condition && tag}
+	{#if typeof tag === 'string'}
+		<svelte:element this={tag} {...$$restProps}><slot /></svelte:element>
+	{:else}
+		<svelte:component this={tag} ...$$restProps><slot /></svelte:component>
+	{/if}
+{:else}
+	<slot />
+{/if}
