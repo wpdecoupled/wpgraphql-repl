15d4250afc3019cf7c61d7d8e0475a3d5464ce64
AJ Moon <alex.jared.moon@gmail.com>
2023-06-13 01:46:49 +0000

    revert

--- site/vite.config.js
+++ site/vite.config.js
@@ -1,21 +1,11 @@
 import { readFileSync } from 'node:fs';
-import { join } from 'node:path';
-import { cwd } from 'node:process';
-
 import { sentrySvelteKit } from '@sentry/sveltekit';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-import fs from 'node:fs';
-
-const pkg = JSON.parse(readFileSync(join(cwd(), 'package.json')));
-
 export default defineConfig({
 	plugins: [sentrySvelteKit(), sveltekit(), rawFonts(['.ttf'])],
 	assetsInclude: ['**/*.php'],
-	ssr: {
-		noExternal: Object.keys(pkg.dependencies || {})
-	},
 	resolve: {
 		build: {
 			sourcemap: true
@@ -28,7 +18,7 @@ function rawFonts(ext) {
 		name: 'vite-plugin-raw-fonts',
 		transform(code, id) {
 			if (ext.some((e) => id.endsWith(e))) {
-				const buffer = fs.readFileSync(id);
+				const buffer = readFileSync(id);
 				return { code: `export default ${JSON.stringify(buffer)}`, map: null };
 			}
 		},
