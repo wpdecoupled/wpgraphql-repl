5198dab6cb5dc9741ed41cde3783ad1c079b48ef
AJ Moon <alex.jared.moon@gmail.com>
2023-06-19 09:51:07 +0000

    playgorund lint & format

--- wpgraphql-playground/src/lib/Playground.svelte
+++ wpgraphql-playground/src/lib/Playground.svelte
@@ -1,29 +1,28 @@
 <script lang="ts">
 	import { createEventDispatcher } from 'svelte';
 	import PlaygroundWindow from './PlaygroundWindow.svelte';
-  import { getKey } from './stores/actions';
-	import { PLAYGROUND_WP_DEFAULT, PLAYGROUND_PHP_DEFAULT, PLAYGROUND_URL_DEFAULT } from  './consts'
+	import { getKey } from './stores/actions';
+	import { PLAYGROUND_WP_DEFAULT, PLAYGROUND_PHP_DEFAULT, PLAYGROUND_URL_DEFAULT } from './consts';
 	import type { SupportedWordPressVersions, SupportedPHPVersions } from './types';
-	import { wpUrl, client } from './stores'
+	import { wpUrl, client } from './stores';
 
 	// export let config: Partial<PlaygroundConfig>;
 
-const dispatch = createEventDispatcher();
-
+	const dispatch = createEventDispatcher();
 
 	export let wpVersion: SupportedWordPressVersions = PLAYGROUND_WP_DEFAULT;
 	export let phpVersion: SupportedPHPVersions = PLAYGROUND_PHP_DEFAULT;
 	export let initialUrl: string = PLAYGROUND_URL_DEFAULT;
-	export let debug: boolean = false;
+	export let debug = false;
 
 	$: if (debug) {
-		console.log("new playground url: ", $wpUrl)
-		console.log("new client", $client)
+		console.log('new playground url: ', $wpUrl);
+		console.log('new client', $client);
 	}
 
-	$: dispatch('newUrl', $wpUrl)
+	$: dispatch('newUrl', $wpUrl);
 
-	$: key = getKey(wpVersion, phpVersion)
+	$: key = getKey(wpVersion, phpVersion);
 </script>
 
 {#key key}
@@ -32,8 +31,8 @@ const dispatch = createEventDispatcher();
 			landingPage: initialUrl,
 			preferredVersions: {
 				wp: wpVersion,
-				php: phpVersion,
-			},
+				php: phpVersion
+			}
 		}}
 	/>
 {/key}
