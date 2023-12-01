9af9a14ab72c32806a0187d0234ee320028e29ad
Alex Moon <moonmeister@users.noreply.github.com>
2023-12-01 05:07:59 +0000

    fix: properly order imports/exports

--- site/src/lib/repl/state/consts.ts
+++ site/src/lib/repl/state/consts.ts
@@ -1,6 +1,7 @@
-export const REPL_NAME_DEFAULT = 'Hello World';
 export {
 	PLAYGROUND_WP_DEFAULT,
 	PLAYGROUND_URL_DEFAULT,
 	PLAYGROUND_PHP_DEFAULT,
 } from 'wpgraphql-playground';
+
+export const REPL_NAME_DEFAULT = 'Hello World';
