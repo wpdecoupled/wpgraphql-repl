d4f1aa994d83ce7eb9d651ef2d1be1e3c22ee6c9
Alex Moon <moonmeister@users.noreply.github.com>
2023-12-01 05:20:59 +0000

    chore: cleanup misc

--- site/src/lib/repl/state/index.ts
+++ site/src/lib/repl/state/index.ts
@@ -1,19 +1,2 @@
-// import { getContext, setContext } from 'svelte';
-
-// import type { ReplContext } from './stores';
-
 export { type ReplStateValue, repl_state } from './stores';
-// export { default as PlaygroundProvider } from './PlaygroundProvider.svelte';
 export * from './consts';
-
-// export const CONTEXT_KEY = Symbol('wp-playground-context');
-
-// //Creates initial context from URL
-// export function setPlaygroundContext(builder: () => ReplContext) {
-// 	setContext<ReplContext>(CONTEXT_KEY, builder());
-// }
-
-// //Returns current context
-// export function getPlaygroundContext(): ReplContext {
-// 	return getContext(CONTEXT_KEY);
-// }
